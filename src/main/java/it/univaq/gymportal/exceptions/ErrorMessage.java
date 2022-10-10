f99d201283826d7bb7daf6c0a4b82a0331708da9
Vincenzo Apostolo <apos.vincenzo@gmail.com>
2022-10-11 00:11:17 +0200

    add exceptions

---
+++ src/main/java/it/univaq/gymportal/exceptions/ErrorMessage.java
@@ -0,0 +1,56 @@
+package it.univaq.gymportal.exceptions;
+
+public class ErrorMessage {
+
+    private int code;
+    private String message;
+
+    public ErrorMessage() {
+    }
+
+    public ErrorMessage(int SQLcode) {
+        System.out.println("[SQL Error Code]: " + SQLcode);
+        switch (SQLcode){
+            case 1062:
+                this.code = 400;
+                this.message = "Data Already Exist";
+                break;
+            case 1366:
+                this.code = 400;
+                this.message = "Incorrect value";
+                break;
+            case 1452:
+                this.code = 400;
+                this.message = "Cannot add or update - The related data doesn't exist";
+                break;
+            default:
+                this.code = 500;
+                this.message = "Server Error - SQLError: " + SQLcode;
+                break;
+        }
+    }
+
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "[Error Message] - " +
+                "Code: " + code +
+                ", Message: '" + message + '\'';
+    }
+}
